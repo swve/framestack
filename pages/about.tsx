d1c9725d2f86dd34d859937e4bd71c369d46addb
swve <bdswve@gmail.com>
2021-05-19 00:35:29 +0200

    💄  add about & contribute page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,87 @@
 import Layout from "../components/layout";
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 const About = (props) => {
   return (
     <Layout title="About" desc="About page">
       <div className="hero-build">
-        <div style={{textAlign:"center"}}>
+        <div style={{ textAlign: "center", marginTop: "120px" }}>
           {" "}
           <br></br>
-          <img src="../img/logo.png"/>
-          
-          <p>
-          <br></br>
-            Framestack helps you choose and find the perfect tool or framework
-            to use to build and start your project, you can find your perfet tool by usage and soon by language, Framestack is an evolution of <b><a target="_blank" href="http://frame.graphicmade.com">Frame</a> </b>
-            it's predecessor {" "}<br></br>
-            Built on Node.js, React, NextJS it means that it's incredibly very fast, SEO ready, Future proof and soon avalaible in mobile 🌀
-            <br></br><br></br>
-            <img width={500} src="https://graphicmade.com/content/images/2020/01/DW1szauXUAEDtnt.jpg"/>
+          <Image
+            alt="Picture of the author"
+            width={120}
+            height={15}
+            src="/img/logo.png"
+          />
+          <h1 style={{ fontSize: "50px" }}>Tools you need to start building</h1>
+          <br />
+          <p
+            style={{
+              margin: "auto",
+              marginTop: "7px",
+              marginBottom: "15px",
+              fontWeight: "bold",
+              fontSize: 17,
+              width: 550,
+            }}
+          >
+            Framestack is a collection of tools/libraries/frameworks you'll
+            probably need to start building your next idea and turn it to a
+            reality{" "}
           </p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              margin: "auto",
+              marginTop: "70px",
+              marginBottom: "70px",
+              width: "80%",
+            }}
+          >
+            <div className="icon-feature" style={{ width: 250 }}>
+              <img width="50" src="/img/frameworks-icon.png"></img>
+              <h2 style={{ marginTop: "5px", margin: "auto" }}>Frameworks</h2>
+              <p>
+                Fully fledged platforms/toolkits you can use and follow to build
+                a specific project{" "}
+              </p>
+            </div>
+            <div className="icon-feature" style={{ width: 250 }}>
+              <img width="53" src="/img/libraries-icon.png"></img>
+              <h2 style={{ marginTop: "5px", margin: "auto" }}>Libraries</h2>
+              <p>
+                Pieces of code, generic code that will help you build your
+                project{" "}
+              </p>
+            </div>
+            <div className="icon-feature" style={{ width: 250 }}>
+              <img width="50" src="/img/tools-icon.png"></img>
+              <h2 style={{ marginTop: "5px", margin: "auto" }}>Tools</h2>
+              <p>
+                Platforms or full software that provide the tooling to build a
+                project{" "}
+              </p>
+            </div>
+          </div>
+          <div style={{ marginBottom: "200px" }}>
+            <p style={{marginTop: "20px",textTransform: "uppercase",margin: "auto"}}>
+              Made by <b>Sweave</b>{" "}
+            </p>
+              <a href="https://github.com/swve">
+                <img
+                  style={{
+                    marginTop: "15px",
+                    width: "50px",
+                    borderRadius: "7px",
+                  }}
+                  src="https://avatars.githubusercontent.com/u/29493708?v=4"
+                  alt=""
+                />
+              </a>
+          </div>
         </div>
       </div>
     </Layout>
