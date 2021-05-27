88d17bfc1870ce516cf5f7971a10e87f8e41ff31
swve <bdswve@gmail.com>
2021-05-27 12:03:38 +0200

    add privacy section

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
 
-const Footer = props => (
+const Footer = (props) => (
   <div className="footer">
-    
     <div className="text-footer">
-      © 2021 
-       <br></br>
-      
+      © 2020 - 2021 | {" "}
+      <Link href="/privacy">
+        <a>Privacy</a>
+      </Link>{" "}
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <br></br>
     </div>
-    <a href="https://github.com/swve" target="_blank" rel="noreferrer" ><img alt="Sweave logo" src="../img/sweave.png"/></a>
+    <a href="https://github.com/swve" target="_blank" rel="noreferrer">
+      <img alt="Sweave logo" src="../img/sweave.png" />
+    </a>
     <br></br>
-   
   </div>
 );
 
