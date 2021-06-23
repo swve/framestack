f4914620ce8843da0deaeeed06272da3ec3bbcda
swve <bdswve@gmail.com>
2021-06-23 18:38:29 +0200

    add bmc link

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,7 +16,9 @@ const Footer = (props) => (
       <img alt="Sweave logo" src="../img/sweave.png" />
     </a>
     <br></br>
-  </div>
+
+    <a href="https://www.buymeacoffee.com/swve"><img style={{width:120,height:"auto",marginTop:7}} src="https://img.buymeacoffee.com/button-api/?text=Buy me a pizza&emoji=🍕&slug=swve&button_colour=BD5FFF&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"/></a>
+ </div>
 );
 
 export default Footer;
