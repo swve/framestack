e9a35de4aae104d85204033c31d99b1fdfed5d95
badr.bennasri <badr.bennasri@ekino.com>
2022-08-09 18:35:38 +0200

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = (props) => (
   <div className="footer">
     <div className="text-footer">
-      © 2020 - 2021 | {" "}
+      © 2020 - 2022 -{" "}
       <Link href="/privacy">
         <a>Privacy</a>
       </Link>{" "}
@@ -13,12 +13,14 @@ const Footer = (props) => (
       <br></br>
     </div>
     <a href="https://github.com/swve" target="_blank" rel="noreferrer">
-      <img alt="Sweave logo" src="../img/sweave.png" />
+      <img className="swve-logo" alt="Sweave logo" src="../img/sweave.png" />
     </a>
     <br></br>
 
-    <a href="https://www.buymeacoffee.com/swve"><img style={{width:120,height:"auto",marginTop:7}} src="https://img.buymeacoffee.com/button-api/?text=Buy me a pizza&emoji=🍕&slug=swve&button_colour=BD5FFF&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"/></a>
- </div>
+    <a href="http://github.com/sponsors/swve" target={"_blank"}>
+      <img alt="GitHub Sponsors" src="https://img.shields.io/github/sponsors/swve?label=Sponsor%20Framestack&style=social" />
+    </a>
+  </div>
 );
 
 export default Footer;
